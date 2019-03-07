3d39f731aef40393ee3cdeea415277522c3bf6d3
Willian Welbert <willianwelbert@yahoo.com.br>
2019-03-07 13:18:54 -0300

    solves challenge 16

--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -1,10 +1,12 @@
-/*
+(function() {
+  /*
+
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
-
-/*
+  "use strict";
+  /*
 Declare uma variável chamada `name` que receba seu primeiro nome.
 Mostre no console todas as letras do seu nome separadas, com a frase:
 - "[LETRA] é a [POSIÇÃO]ª letra do meu nome."
@@ -13,10 +15,14 @@ Ex: no caso do nome ser "Fernando", deve mostrar as frases:
 - "e é a 2ª letra do meu nome."
 E assim por diante, até a última.
 */
-console.log( 'As letras do seu nome:' );
-// ?
+  console.log("As letras do seu nome:");
+  var name = "Willian";
+  var splitName = name.split("");
+  for (var i = 0; i < splitName.length; i++) {
+    console.log(`${splitName[i]} é a ${i + 1}ª letra do meu nome`);
+  }
 
-/*
+  /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
 escrito no formato de slug (caixa baixa e palavras separadas por um traço).
 Ex: o nome "Fernando Daciuk" ficaria "fernando-daciuk"
@@ -28,10 +34,25 @@ curso para fazer isso funcionar corretamente :)
 - Mostre no console o nome no formato slug, e o resultado final. Use um
 console.log para cada formato.
 */
-console.log( '\nNome convertido à partir de um slug:' );
-// ?
+  console.log("\nNome convertido à partir de um slug:");
+  var fullName = "willian-welbert";
+  console.log("nome em slug", fullName);
 
-/*
+  function clearName(slugedname) {
+    var separetedName = slugedname.split("-");
+    var clearName = [];
+    for (var i = 0; i < separetedName.length; i++) {
+      clearName.push(
+        separetedName[i][0].toUpperCase(),
+        separetedName[i].slice(1),
+        " "
+      );
+    }
+    return clearName.join("");
+  }
+  console.log("nome limpo", clearName(fullName));
+
+  /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
 cada nome por vírgula. Entre o penúltimo e o último nome, o separador deve
 ser um "e". No final, concatene com a frase: " são meus amigos.".
@@ -40,24 +61,35 @@ O resultado final deve ficar mais ou menos assim:
 - Detalhe: o código abaixo deve funcionar com um array de qualquer tamanho.
 5 nomes foi somente uma sugestão ;)
 */
-console.log( '\nMeus amigos:' );
-// ?
+  console.log("\nMeus amigos:");
+  var friends = ["Alexandre", "Marcos", "Artur", "Thaiane", "Roger"];
+  var stringfriends = friends.join(", ");
+  var lastFriend = stringfriends.substring(stringfriends.lastIndexOf(","));
+  var lastFriendCorrect = stringfriends
+    .substring(stringfriends.lastIndexOf(","))
+    .replace(",", " e");
 
-/*
+  console.log(
+    stringfriends.replace(lastFriend, lastFriendCorrect),
+    "são meus amigos"
+  );
+
+  /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
 Mostre o resultado no console.
 */
-console.log( '\nEra "Roberto", agora é:' );
-// ?
+  console.log('\nEra "Roberto", agora é:');
+  console.log("Roberto".replace("to", "ta"));
 
-/*
+  /*
 Mostre no console a parte "nando" da string "Fernando". Use o método que
 faz a busca do final para o início da string.
 */
-console.log( '\nParte de uma string:' );
-// ?
+  console.log("\nParte de uma string:");
+  var nando = "Fernando".lastIndexOf("nando");
+  console.log("Fernando".slice(nando));
 
-/*
+  /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
 escrito de forma natural.
 Mostre no console esse nome, mas com as letras intercalando entre maiúsculas
@@ -66,5 +98,20 @@ e minúsculas.
 de qualquer tamanho, escrito de qualquer forma.
 Ex.: Nomes que deveriam funcionar: "Fernando", "RoBertO", "gabriEla", etc.
 */
-console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
-// ?
+  "we".toLowerCase;
+
+  console.log("\nNome com letras intercaladas entre caixa alta e baixa:");
+  var myName = "Willian";
+  function orkutSpelling(name) {
+    var orkutName = [];
+    name
+      .toLowerCase()
+      .split("")
+      .map(function(letter, index) {
+        orkutName.push(index % 2 === 0 ? letter.toUpperCase() : letter);
+      });
+    return orkutName.join("");
+  }
+
+  console.log(orkutSpelling(myName));
+})();
