0f65b6e6440132520f7896089bdfa7409ef2cac8
Willian Welbert <willianwelbert@yahoo.com.br>
2019-04-01 18:26:19 -0300

    continues challenge 23, starts challenge 24

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -26,14 +26,85 @@
   - Ao pressionar o botão "CE", o input deve ficar zerado.
   */
 
-  var buttonZero = doc.querySelector("[data-calc='0']");
-  var buttonOne = doc.querySelector("[data-calc='1']");
-  var buttonTwo = doc.querySelector("[data-calc='2']");
-  var buttonThree = doc.querySelector("[data-calc='3']");
-  var buttonFour = doc.querySelector("[data-calc='4']");
-  var buttonFive = doc.querySelector("[data-calc='5']");
-  var buttonSix = doc.querySelector("[data-calc='6']");
-  var buttonSeven = doc.querySelector("[data-calc='7']");
-  var buttonEight = doc.querySelector("[data-calc='8']");
-  var buttonNine = doc.querySelector("[data-calc='9']");
+  //all buttons
+  var $numbers = doc.querySelectorAll(".number");
+
+  //buttons signs
+  var $buttonErase = doc.querySelector("[data-calc='erase']");
+  var $buttonEquals = doc.querySelector("[data-calc='equals']");
+  var $operators = doc.querySelectorAll(".operator");
+  // signs end
+
+  var $display = doc.querySelector("[data-calc='screen']");
+
+  var totalNumbers = $numbers.length;
+
+  for (var i = 0; i < totalNumbers; i++) {
+    $numbers[i].addEventListener(
+      "click",
+      function() {
+        $display.value += this.getAttribute("data-calc");
+      },
+      false
+    );
+  }
+
+  var totalOperators = $operators.length;
+
+  for (var i = 0; i < totalOperators; i++) {
+    $operators[i].addEventListener(
+      "click",
+      function() {
+        var lastInput = $display.value.charAt($display.value.length - 1);
+
+        if (lastInput.match(/[-+*/]/)) {
+          var notLastOperator = $display.value.length - 1;
+          $display.value = $display.value
+            .substring(0, notLastOperator)
+            .concat(this.innerText);
+        } else {
+          $display.value += this.innerText;
+        }
+      },
+      false
+    );
+  }
+
+  var stringToOperator = {
+    "+": function(x, y) {
+      return +x + +y;
+    },
+    "-": function(x, y) {
+      return +x - +y;
+    },
+    "*": function(x, y) {
+      return +x * +y;
+    },
+    "/": function(x, y) {
+      return +x / +y;
+    }
+  };
+
+  $buttonErase.addEventListener(
+    "click",
+    function() {
+      $display.value = "";
+    },
+    false
+  );
+
+  $buttonEquals.addEventListener(
+    "click",
+    function() {
+      //calculation logic
+      var valuesArray = $display.value.split(/\D/);
+      console.log(valuesArray);
+      console.log(stringToOperator["+"](valuesArray[0], valuesArray[1]));
+      var operatorsArray = $display.value.split(/\d/g).filter(item => {
+        return item !== "";
+      });
+      console.log(operatorsArray);
+    },
+    false
+  );
 })(window, document);
